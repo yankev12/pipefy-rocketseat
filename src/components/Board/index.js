abd0245d3ca369eca77704b7444f3a726d9fabc7
yankev <yankevmineo@gmail.com>
2020-09-14 23:05:16 -0300

    First Commit

---
+++ src/components/Board/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from '../List';
+import { LoadLists, loadLists } from '../../services/api'
+import { Container } from './styles';
+
+const lists = loadLists();
+
+export default function Board() {
+    return (
+        <Container>
+            {lists.map(list => <List key={list.title} data={list} />)}
+        </Container>
+    );
+}
